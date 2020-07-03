43c5788ecd523e10813e3c9db5408b4fb8211c1c
yanghyunjun <akakak231@gmail.com>
2020-07-03 23:27:25 +0900

    [add] babel-plugin-inline-react-svg

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { NextPage } from "next";
+
+const Container = styled.div``;
+
+const index: NextPage = () => {
+  return <Container>hello world</Container>;
+};
+
+export default index;
